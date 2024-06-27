f2433d8b9ac6e3620bd137bf64fc59dfa258df7f
Mhlokza <123315705+Mhlokza@users.noreply.github.com>
2024-06-27 14:38:14 +0000

    annual seving culculations

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,7 @@
 monthly_income = int(input("Enter your monthly income: "))
-monthly_expense = int(input( "Enter your total monthly expenses: "))
-contain monthly_savings=(monthly_income-monthly_expenses(float)(monthly_income)-(float)monthly_expenses))
-projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
-print("Projected savings after one year, with interest, is: $",int(projected_savings)) 
+monthly_expenses = int(input( "Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+annual_interest_rate= 0.05
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * annual_interest_rate)
+print("Your monthly savings are $", monthly_savings)
+print("Projected savings after one year, with interest, is: $",int(projected_savings))
