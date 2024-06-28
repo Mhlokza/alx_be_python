0e699df2bec10ab114f1301cb405e7463526762f
Mhlokza <123315705+Mhlokza@users.noreply.github.com>
2024-06-28 17:04:06 +0000

    parten drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,6 +2,6 @@ integer = int(input("Enter the size of the pattern: "))
 i=1
 while i<=integer:
     for ans in range(0,integer):
-        print("*"*integer, end="")
-        i=i+1
-        print()
+        print("*", end="")
+    i=i+1
+    print()
