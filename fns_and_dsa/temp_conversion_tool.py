73ece89f33f1c7e0fd3f55269b8fdfb62f2d0caf
Mhlonipeni <mhlonipheni9@gmail.com>
2024-07-05 17:00:23 +0200

    temparature convession

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,19 +1,18 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
-temperature=0
 def convert_to_celsius(fahrenheit):
-    celcius = (temperature-32) * FAHRENHEIT_TO_CELSIUS_FACTOR
-    print(f"{temperature}°F is {celcius}°C")
+    celcius = (fahrenheit-32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    print(f"{fahrenheit}°F is {celcius}°C")
 
 def convert_to_fahrenheit(celsius):
-    fahrenheit= CELSIUS_TO_FAHRENHEIT_FACTOR*temperature+32
-    print(f"{temperature}°C is {fahrenheit}°F")
+    fahrenheit= (CELSIUS_TO_FAHRENHEIT_FACTOR*celsius)+32
+    print(f"{celsius}°C is {fahrenheit}°F")
 
 temperature = float(input("Enter the temperature to convert:"))
 unit = input("Is this temperature in Celsius or Fahrenheit? (C/F):").upper()
 if unit== "C":
-    convert_to_celsius(fahrenheit=temperature)
+    print(convert_to_celsius(fahrenheit=temperature))
 elif unit=="F":
-    convert_to_fahrenheit(celsius=temperature)
+    print(convert_to_fahrenheit(celsius=temperature))
 else:
     print("enter valid unit and temperature")
