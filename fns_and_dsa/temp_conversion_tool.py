1f189e7e8a527e7aaf86b528a421fb05630c9a6a
Mhlonipeni <mhlonipheni9@gmail.com>
2024-07-05 22:45:17 +0200

    temperature convession

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,11 +1,10 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return f"{fahrenheit}°F is {celsius}°C"
-
 def convert_to_fahrenheit(celsius):
-    fahrenheit = (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
+    fahrenheit = ((celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32)
     return f"{celsius}°C is {fahrenheit}°F"
 
 temperature = float(input("Enter the temperature to convert: "))
