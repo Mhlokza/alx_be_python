c01d1cfb8152e05a2ca20d01dfa442a8a74e8ecf
Mhlokza <123315705+Mhlokza@users.noreply.github.com>
2024-07-10 16:55:25 +0000

    bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self,account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self,amount):
+        self.account_balance+=amount
+
+    def withdraw(self,amount):
+        self.account_balance-=amount
+
+        if self.account_balance>=amount:
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        print(f"{self.account_balance:.2f}")
