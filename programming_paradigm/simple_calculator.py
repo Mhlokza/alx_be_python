be7bdb19807eb06023cf66607dea38aa88291abb
Mhlokza <123315705+Mhlokza@users.noreply.github.com>
2024-07-11 22:08:06 +0000

    test cases of a culculator

---
+++ programming_paradigm/simple_calculator.py
@@ -0,0 +1,29 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestCase(unittest.TestCase):
+    def test_add(self):
+        result = SimpleCalculator.add(0,10,5)
+        self.assertEqual(result,15)
+
+    def test_subr(self):
+        result = SimpleCalculator.subtract(0,10,5)
+        self.assertEqual(result,5)
+
+    def test_mult(self):
+        result = SimpleCalculator.multiply(0,10,5)
+        self.assertEqual(result,50)
+
+    def test_div(self):
+        result = SimpleCalculator.divide(0,10,5)
+        self.assertEqual(result,2)
+
+    def test_div_by_0(self):
+       try:
+        result = SimpleCalculator.divide(0,10,0)
+        self.assertEqual(result,None)
+       except ZeroDivisionError as e:
+           print(e)
+
+       finally:
+           print("you cannot divide by zero")
