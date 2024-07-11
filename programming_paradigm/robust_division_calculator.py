c65f96184a9ba1b0041c7a8e2ef8e7db19fb655c
Mhlokza <123315705+Mhlokza@users.noreply.github.com>
2024-07-11 17:39:25 +0200

    division error exception

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,15 +1,13 @@
 def safe_divide(numerator, denominator):
-    if denominator == 0:
-        try:
-            numerator/denominator
-        except ZeroDivisionError:
-            print("Error: Cannot divide by zero.")
-        else:
-            return f"The result of the division is {numerator/denominator}"
-
-numerator = input("enter the value: ")
-denominator= input("enter the value: ")
-try:
-    number = float(numerator), float(denominator)
-except ValueError as e:
-    print("Error: Please enter numeric values only.")
+    try:
+        numerator = float(numerator)
+        denominator = float(denominator)
+        result = numerator/denominator
+    except ValueError:
+        print("Error: Please enter numeric values only.") 
+        quit()
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+        quit()
+    return f"The result of the division is {result}"
+    
