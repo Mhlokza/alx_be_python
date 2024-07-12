39179e90b6e4b08572057e8bf91159032e0d3382
Mhlokza <123315705+Mhlokza@users.noreply.github.com>
2024-07-12 06:11:35 +0000

    test cases of culcultion

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,24 +4,24 @@ from simple_calculator import SimpleCalculator
 class TestCase(unittest.TestCase):
     def test_addition(self):
         result = SimpleCalculator().add(10,5)
-        self.assertEqual(result,15)
+        self.assertEqual(self,result,15)
 
     def test_subraction(self):
         result = SimpleCalculator().subtract(10,5)
-        self.assertEqual(result,5)
+        self.assertEqual(self,result,5)
 
     def test_multiplication(self):
         result = SimpleCalculator().multiply(10,5)
-        self.assertEqual(result,50)
+        self.assertEqual(self,result,50)
 
     def test_divesion(self):
         result = SimpleCalculator().divide(10,5)
-        self.assertEqual(result,2)
+        self.assertEqual(self,result,2)
 
     def test_divesion_by_0(self):
        try:
         result = SimpleCalculator().divide(10,0)
-        self.assertEqual(result,None)
+        self.assertEqual(self,result,None)
        except ZeroDivisionError as e:
            print(e)
 
