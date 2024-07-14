b73571a42cc94ced3fdf1fe17a95c90b34e32945
Mhlokza <123315705+Mhlokza@users.noreply.github.com>
2024-07-14 18:48:54 +0000

    book collection

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,23 +1,34 @@
-class Books:
+class Book:
     def __init__(self,title,author):
-        self.title= title
-        self.author= author
-        self._is_checked_out=True
+        self.tittle=title
+        self.author=author
+        self._is_checked_out = True
 
-class Library(Books):
-    _books =[]
-    def add_book(self):
-        self._books.append(book)
+class Library:
+    def __int__(self):
+        self._books=[]
+        self._books={}
+        self.checkouts = {}
+
+    def add_book(self,title_and_book):
+        tittle_book_dict = {title_and_book.title: title_and_book.author}
+        self._books.update(tittle_book_dict)
 
     def check_out_book(self,title):
-        self._books.remove(title)
+        if title in self._books:
+            title_book_dict = {title:self._books[title]}
+            self.checkouts.update(title_book_dict)
+            self._books.pop(title)
+    def return_book(self,title):
+        book_in_checkout = title in self.checkouts
+        if book_in_checkout==True:
+            title_book_dict ={title : self.checkouts[title]}
+            self._books.update(title_book_dict)
+            self.checkouts.pop(title)
 
     def list_available_books(self):
-        if self._books==self.title:
-            self._books+=book
-            print(f" Available books after checking out {self._books}")
-
-    def return_book(self,title):
-        if self._books != title:
-            self._books+=book
+        for book_title in self._books:
+            output_text = []
+            output_text.append(book_title, self._books[book_title])
+            print("".join(output_text))
 
