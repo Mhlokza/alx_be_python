f8807f7015ad20a833864fafa1d6186f955c0bb9
Mhlokza <123315705+Mhlokza@users.noreply.github.com>
2024-07-17 16:56:32 +0000

    library book

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,7 +3,7 @@ class Book:
         self.title = title
         self.author = author
     def __str__(self):
-        return f"Book: {self.title} by {self.author} "
+        print(f"Book: {self.title} by {self.author}")
 
 class EBook(Book):
     def __init__(self,title,author,file_size):
@@ -11,14 +11,14 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+        print(f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB")
 
 class PrintBook(Book):
     def __init__(self,title,author,page_count):
         super().__init__(title,author)
         self.page_count = page_count
     def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+        print(f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}")
 
 class Library:
     def __init__(self):
