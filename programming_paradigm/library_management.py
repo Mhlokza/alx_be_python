6e536eac3f41c661586cb08534dc6070d04238d4
Mhlokza <123315705+Mhlokza@users.noreply.github.com>
2024-07-20 06:53:14 +0000

    library manangement

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,34 +1,20 @@
 class Book:
     def __init__(self,title,author):
-        self.tittle=title
-        self.author=author
+        self.title = title
+        self.author = author
         self._is_checked_out = True
-
+_books  = []
 class Library:
-    def __int__(self):
-        self._books=[]
-        self._books={}
-        self.checkouts = {}
 
-    def add_book(self,title_and_book):
-        tittle_book_dict = {title_and_book.title: title_and_book.author}
-        self._books.update(tittle_book_dict)
+    def add_book(self,book):
+        _books.append(book)
 
-    def check_out_book(self,title):
-        if title in self._books:
-            title_book_dict = {title:self._books[title]}
-            self.checkouts.update(title_book_dict)
-            self._books.pop(title)
+    def  check_out_book(self,title):
+        if title == _books:
+            _books.remove()
+
     def return_book(self,title):
-        book_in_checkout = title in self.checkouts
-        if book_in_checkout==True:
-            title_book_dict ={title : self.checkouts[title]}
-            self._books.update(title_book_dict)
-            self.checkouts.pop(title)
-
+        if title != _books:
+            _books.append()
     def list_available_books(self):
-        for book_title in self._books:
-            output_text = []
-            output_text.append(book_title, self._books[book_title])
-            print("".join(output_text))
-
+        return _books
